5bf34b1f44ab77c695afa329bb06063d3f8efc00
Akın Ünver <66174369+akinunver97@users.noreply.github.com>
2025-10-11 15:48:53 +0300

    hierarchy modified

--- learning_ps/thrash/dowhileloop.ps1
+++
@@ -1,8 +0,0 @@
-$Xmen = @('Wolverine', 'Cyclops', 'Storm', 'Professor X', 'Gambit', 'Dr. Jean Grey')
-$counter = 0
-
-Write-Host "This is for do while loop" -ForegroundColor Red
-Do {
-    Write-Host $Xmen[$counter] "is a mutant!"
-    $counter++
-} While ($counter -lt $Xmen.Length)
